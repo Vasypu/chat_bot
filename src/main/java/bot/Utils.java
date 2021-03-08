c7887efdb48debeb733963c1e3789ab19e5fb245
Vasypu <lesch.kostenko@yandex.ru>
2021-03-08 20:19:34 +0200

    Initial commit

---
+++ src/main/java/bot/Utils.java
@@ -0,0 +1,9 @@
+package bot;
+
+import org.apache.commons.validator.EmailValidator;
+
+public class Utils {
+    public static boolean isValidEmailAddress(String email) {
+        return EmailValidator.getInstance().isValid(email);
+    }
+}
